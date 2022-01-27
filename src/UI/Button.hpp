c5c257792cb3ed361f75f65403ccee7b6a1f11a0
Jesus Cantu <chuy_cr11@hotmail.com>
2022-01-27 17:21:17 -0600

    Created basic GUI classes (implementation pending)

---
+++ src/UI/Button.hpp
@@ -0,0 +1,10 @@
+#ifndef __BUTTON_H__
+#define __BUTTON_H__
+
+#include "Widget.hpp"
+
+class Button : public Widget {
+    
+}
+
+#endif // __BUTTON_H__
---
+++ src/UI/Checkbox.hpp
@@ -0,0 +1,5 @@
+#ifndef __CHECKBOX_H__
+#define __CHECKBOX_H__
+
+
+#endif // __CHECKBOX_H__
