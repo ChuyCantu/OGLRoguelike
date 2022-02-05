ba592449881b4797a382feaee8a2afe879378f48
Jesus Cantu <chuy_cr11@hotmail.com>
2022-02-05 01:43:21 -0600

    Merged both text loading functions since they were almost identical

--- src/UI/Text/TextRenderer.cpp
+++ src/UI/Text/TextRenderer.cpp
@@ -17,7 +17,12 @@
 
 FontMap TextRenderer::fonts;
 
-void TextRenderer::LoadFont(const std::string& name, int fontSize, const std::string& fontFile) {
+void TextRenderer::LoadFont(const std::string& fontFile, const std::string& name, int fontSize, FontRenderMode renderMode) {
+    if (fontSize == 0) {
+        LOG_WARN("Could not create font with a size of 0.");
+        return;
+    }
+    
     FT_Library ft;
 
     if (FT_Init_FreeType(&ft)) {
@@ -42,92 +47,28 @@ void TextRenderer::LoadFont(const std::string& name, int fontSize, const std::st
     atlas.size = glm::ivec2{padding, padding * 2}; // Initial size
     atlas.baseFontSize = fontSize;
     atlas.characters = std::vector<CharacterInfo>(charactersNum);
+    atlas.maxBearing = glm::ivec2{0};
 
     std::vector<std::vector<uint8_t>> glyphsBuffer(charactersNum);
 
-    // Caracters with code lower than 32 are control characters only
-    for (uint8_t c {32}; c < charactersNum; ++c) {
-        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-            LOG_WARN("Could not load character '{}'.", c);
-            return;
-        }
-
-        atlas.size.x += face->glyph->bitmap.width + padding;
-        atlas.size.y = std::max((uint32_t)atlas.size.y, face->glyph->bitmap.rows + padding * 2);
-
-        // Temporarily cache glyph buffers
-        uint32_t bufferSize {face->glyph->bitmap.width * face->glyph->bitmap.rows};
-        glyphsBuffer[c] = std::vector<uint8_t>(bufferSize);
-        memcpy(glyphsBuffer[c].data(), face->glyph->bitmap.buffer, bufferSize);
-
-        // Fill initial character info
-        atlas.characters[c].size = glm::ivec2{face->glyph->bitmap.width, face->glyph->bitmap.rows};
-        atlas.characters[c].bearing = glm::ivec2{face->glyph->bitmap_left, face->glyph->bitmap_top};
-        atlas.characters[c].advance = glm::ivec2{face->glyph->advance.x, face->glyph->advance.y};
-    }
-
-    atlas.texture = MakeRef<Texture>();
-    atlas.texture->Generate(atlas.size.x, atlas.size.y, nullptr, TextureFormat::R8, TextureFormat::RED);
-    atlas.texture->SetWrapS(TextureParameter::ClampToEdge).SetWrapT(TextureParameter::ClampToEdge).
-                SetMinFilter(TextureParameter::Linear).SetMagFilter(TextureParameter::Linear);
-
-    // Generate texture atlas and set characters uv coords (for now is a single row, so y is always = padding)
-    uint32_t x {static_cast<uint32_t>(padding)};
-    for (uint8_t c{32}; c < 128; ++c) {
-        atlas.characters[c].uv = glm::vec2{(float)x / atlas.size.x, padding};
-
-        // Upload glyph buffer to atlas texture
-        atlas.texture->SubImage(x, padding, atlas.characters[c].size.x, atlas.characters[c].size.y, glyphsBuffer[c].data());
-
-        x += atlas.characters[c].size.x + padding;
-    }
-
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-
-    FT_Done_Face(face);
-    FT_Done_FreeType(ft);
-
-    fonts[name][fontSize] = atlas;
-}
-
-void TextRenderer::LoadFontSDF(const std::string& name, const std::string& fontFile, int fontBaseSize) {
-    FT_Library ft;
-
-    if (FT_Init_FreeType(&ft)) {
-        LOG_WARN("Could not initialize FreeType library.");
-        return;
-    }
-
-    FT_Face face;
-    if (FT_New_Face(ft, fontFile.c_str(), 0, &face)) {
-        LOG_WARN("Could not open font {}.", fontFile);
-        return;
-    }
-
-    FT_Set_Pixel_Sizes(face, 0, fontBaseSize);
-
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    int charactersNum {128}; // Hardcoded in here because for now this only works with ASCII
-    int padding {2};
-    
-    Atlas atlas;
-    atlas.size = glm::ivec2{padding, padding * 2}; // Initial size
-    atlas.baseFontSize = fontBaseSize;
-    atlas.characters = std::vector<CharacterInfo>(charactersNum);
-
-    std::vector<std::vector<uint8_t>> glyphsBuffer(charactersNum);
-
-    // Caracters with code lower than 32 are control characters only
+    // Caracters with code lower than 33 are control characters only
     for (uint8_t c {33}; c < charactersNum; ++c) {
-        if (FT_Load_Char(face, c, FT_LOAD_DEFAULT)) {
-            LOG_CRITICAL("Could not load character '{}'.", c);
-            return;
-        }
+        if (renderMode == FontRenderMode::SDF) {
+            if (FT_Load_Char(face, c, FT_LOAD_DEFAULT)) {
+                LOG_CRITICAL("Could not load character '{}'.", c);
+                // return;
+            }
 
-        if (FT_Render_Glyph(face->glyph, FT_Render_Mode::FT_RENDER_MODE_SDF)) {
-            LOG_CRITICAL("Could not load character '{}' as SDF.", c);
-            return;
+            if (FT_Render_Glyph(face->glyph, FT_Render_Mode::FT_RENDER_MODE_SDF)) {
+                LOG_CRITICAL("Could not load character '{}' as SDF.", c);
+                // return;
+            }
+        }
+        else if (renderMode == FontRenderMode::Raster) {
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+                LOG_CRITICAL("Could not load character '{}'.", c);
+                // return;
+            }
         }
 
         atlas.size.x += face->glyph->bitmap.width + padding;
@@ -142,7 +83,13 @@ void TextRenderer::LoadFontSDF(const std::string& name, const std::string& fontF
         atlas.characters[c].size = glm::ivec2{face->glyph->bitmap.width, face->glyph->bitmap.rows};
         atlas.characters[c].bearing = glm::ivec2{face->glyph->bitmap_left, face->glyph->bitmap_top};
         atlas.characters[c].advance = glm::ivec2{face->glyph->advance.x, face->glyph->advance.y};
+
+        atlas.maxBearing.x = std::max(atlas.maxBearing.x, face->glyph->bitmap_left);
+        atlas.maxBearing.y = std::max(atlas.maxBearing.y, face->glyph->bitmap_top);
     }
+
+    atlas.metricsWidth = face->size->metrics.max_advance;
+    atlas.metricsHeight = face->size->metrics.height;
 
     atlas.texture = MakeRef<Texture>();
     atlas.texture->Generate(atlas.size.x, atlas.size.y, nullptr, TextureFormat::R8, TextureFormat::RED);
@@ -165,22 +112,26 @@ void TextRenderer::LoadFontSDF(const std::string& name, const std::string& fontF
     FT_Done_Face(face);
     FT_Done_FreeType(ft);
 
-    fonts[name][0] = atlas; // Size 0 is reserved for SDF 
-
-    // Temp
-    AssetManager::AddTexture("tempAtlas", atlas.texture);
+    switch (renderMode) {
+        case FontRenderMode::SDF:
+            fonts[name][0] = atlas; // Size 0 index is reserved for SDF 
+            break;
+        case FontRenderMode::Raster:
+            fonts[name][fontSize] = atlas;
+            break;
+    }
 }
 
 void TextRenderer::RenderText(const std::string& text, float size, const glm::vec2& position, const TextInfo& textInfo, const Font& font) {
     auto shader {AssetManager::GetShader("text")};
-    if (font.mode == AtlasMode::SDF) {
+    if (font.mode == FontRenderMode::SDF) {
         shader = AssetManager::GetShader("textSDF");
     }
     shader->Use();
 
     Atlas* atlas;
 
-    if (font.mode == AtlasMode::Bitmap) {
+    if (font.mode == FontRenderMode::Raster) {
         auto fontIter {fonts.find(font.name)};
         if (fontIter != fonts.end()) {
             auto atlasIter {fontIter->second.find(font.size)};
@@ -216,44 +167,51 @@ void TextRenderer::RenderText(const std::string& text, float size, const glm::ve
 
     atlas->texture->Use();
 
+    glm::vec2 scale {size / (float)atlas->baseFontSize};
+
     glEnable(GL_BLEND);
+    glEnable(GL_CULL_FACE);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    float x {position.x};
-    float y {position.y};
+    float penx {position.x};
+    float peny {position.y + atlas->maxBearing.y * scale.y};
     TextBatch::Start();
     for (auto c {text.begin()}; c != text.end(); ++c) {
         CharacterInfo& ch {atlas->characters[static_cast<uint8_t>(*c)]};
 
-        glm::vec2 scale {size / (float)atlas->baseFontSize};
-
-        if (*c == '\n') {
-            x = position.x;
-            y -= atlas->characters['b'].size.y * scale.y;
-            continue;
-        } else if (*c == '\t') {
-            x += atlas->characters['H'].size.x * 4;
-            continue;
+        switch (*c) {
+            case ' ':
+                penx += textInfo.wordSpacing + (atlas->metricsWidth / 2 >> 6) * scale.x;
+                continue;
+            case '\n':
+                penx = position.x;
+                peny += textInfo.lineSpacing + (atlas->metricsHeight >> 6) * scale.y;
+                continue;
+            case '\t':
+                penx += (textInfo.wordSpacing + (atlas->metricsWidth / 2 >> 6) * scale.x) * textInfo.tabSpaces;
+                continue;
         }
 
-        float xpos {x + ch.bearing.x * scale.x};
-        float ypos {-y};
+        // Positions relative to the origin line for the glyph:
+        float xpos {penx + ch.bearing.x * scale.x};
+        float ypos {peny - ch.bearing.y * scale.y};
+
         float w    {ch.size.x * scale.x};
         float h    {ch.size.y * scale.y};
 
         TextVertex tl{
-            glm::vec4{xpos, -ypos - h,
+            glm::vec4{xpos, ypos,
                       ch.uv.x, ch.uv.y / (float)atlas->size.y},
         };
         TextVertex tr{
-            glm::vec4{xpos + w, -ypos - h,
+            glm::vec4{xpos + w, ypos,
                       ch.uv.x + ch.size.x / (float)atlas->size.x, ch.uv.y / (float)atlas->size.y},
         };
         TextVertex bl {
-            glm::vec4{xpos, -ypos, 
+            glm::vec4{xpos, ypos + h, 
                       ch.uv.x, (ch.uv.y + ch.size.y) / (float)atlas->size.y},
         };
         TextVertex br {
-            glm::vec4{xpos + w, -ypos, 
+            glm::vec4{xpos + w, ypos + h, 
                       ch.uv.x + ch.size.x / (float)atlas->size.x, (ch.uv.y + ch.size.y) / (float)atlas->size.y},
         };
 
@@ -272,10 +230,11 @@ void TextRenderer::RenderText(const std::string& text, float size, const glm::ve
 
         TextBatch::AddCharacter(bl, br, tl, tr);
 
-        x += (ch.advance.x >> 6) * scale.x;  // Bitshift by 6 to get a value in pixels (1/64th times 2^6 = 64)
-        y += (ch.advance.y >> 6) * scale.y; 
+        penx += textInfo.letterSpacing + (ch.advance.x >> 6) * scale.x;  // Bitshift by 6 to get a value in pixels (1/64th times 2^6 = 64)
+        peny += (ch.advance.y >> 6) * scale.y; 
     }
     TextBatch::Flush();
+    glDisable(GL_CULL_FACE);
     glDisable(GL_BLEND);
 }
 
@@ -292,6 +251,10 @@ void DebugTextInfoWindow(const std::string& label, TextInfo& textInfo) {
     ImGui::InputFloat("borderWidth", &textInfo.borderWidth);
     ImGui::InputFloat("borderEdge", &textInfo.borderEdge);
     ImGui::InputFloat2("borderOffset", glm::value_ptr(textInfo.borderOffset));
-    DebugColorRGB("bordeColor", textInfo.outlineColor);
+    DebugColorRGB("borderColor", textInfo.outlineColor);
+    ImGui::InputFloat("letterSpacing", &textInfo.letterSpacing);
+    ImGui::InputFloat("wordSpacing", &textInfo.wordSpacing);
+    ImGui::InputFloat("lineSpacing", &textInfo.lineSpacing);
+    ImGui::InputInt("tabSpaces", &textInfo.tabSpaces);
     ImGui::End();
 }
--- src/UI/Text/TextRenderer.hpp
+++ src/UI/Text/TextRenderer.hpp
@@ -9,15 +9,15 @@
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
 
-enum class AtlasMode {
+enum class FontRenderMode {
     SDF,
-    Bitmap
+    Raster
 };
 
 struct Font {
     std::string name;
     int size;
-    AtlasMode mode;
+    FontRenderMode mode;
 };
 
 struct CharacterInfo {
@@ -32,6 +32,10 @@ struct Atlas {
     glm::ivec2 size;
     int baseFontSize;
     std::vector<CharacterInfo> characters;
+
+    int metricsWidth;
+    int metricsHeight;
+    glm::ivec2 maxBearing;
 };
 
 struct TextGradient {
@@ -41,8 +45,19 @@ struct TextGradient {
     Color bottomRightColor;
 };
 
+enum class TextStyle {
+    // Bold,    // This can be manually done
+    // Italics, // This can depend on the font
+    // Underline, // TODO
+    // 
+    Lowecase,
+    Uppercase,
+    SmallCaps
+};
+
 struct TextInfo {
     Color color; 
+    
     bool useColorGradient   {false};                                
     TextGradient colorGradient;
     float width             {0.45f};  
@@ -51,17 +66,33 @@ struct TextInfo {
     float borderEdge        {0.1f}; //! Don't make this 0
     glm::vec2 borderOffset  {0.0f}; // Useful for drop shadows
     Color outlineColor;
+
+    float letterSpacing     {0};
+    float lineSpacing       {0};
+    float wordSpacing       {0};
+    int tabSpaces           {4};
+
+    // bool wrap            {false};
+};
+
+enum class TextHorzAlign {
+    Left, Center, Right
+};
+
+enum class TextVerticalAlign {
+    Top, Center, Bottom
 };
 
 using FontAtlasMap = std::unordered_map<int, Atlas>;
 using FontMap = std::unordered_map<std::string, FontAtlasMap>;
 
+// TODO: Support caching the atlas texture on the computer
 class TextRenderer {
 public:
-    static void Init();
-
-    static void LoadFont(const std::string& name, int fontSize, const std::string& fontFile);
-    static void LoadFontSDF(const std::string& name, const std::string& fontFile, int fontBaseSize = 64);
+    // When renderMode is SDF fontSize is recommended to be left as 64 since bigger sizes take longer to load and 64 is already produces really good results
+    static void LoadFont(const std::string& fontFile, const std::string& name, int fontSize = 64, FontRenderMode renderMode = FontRenderMode::SDF);
+    
+    // Text position represents the top-left point of the bounding rectangle position, so it's easier to work with UI widgets
     static void RenderText(const std::string& text, float size, const glm::vec2& position, const TextInfo& textInfo, const Font& font);
     
 private: 
